90fc423a0544859fb9bd50a8530f84df703c8ad3
Lukian <lukian55d@gmail.com>
2026-01-28 18:01:36 +0000

    done task1

--- db/models.py
+++ db/models.py
@@ -46,7 +46,8 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="orders"
     )
 
     class Meta:
@@ -59,10 +60,14 @@ class Order(models.Model):
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(
-        to=CinemaHall, on_delete=models.CASCADE, related_name="movie_sessions"
+        to=CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
     )
     movie = models.ForeignKey(
-        to=Movie, on_delete=models.CASCADE, related_name="movie_sessions"
+        to=Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
     )
 
     def __str__(self) -> str:
@@ -72,11 +77,13 @@ class MovieSession(models.Model):
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
         "MovieSession",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="tickets"
     )
     order = models.ForeignKey(
         "Order",
         on_delete=models.CASCADE,
+        related_name="tickets"
     )
     row = models.IntegerField()
     seat = models.IntegerField()
--- services/order.py
+++ services/order.py
@@ -5,35 +5,33 @@ from django.utils.dateparse import parse_datetime
 
 from db.models import Order, Ticket
 
-
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None,
 ) -> Order:
-    user = get_user_model()
+    User = get_user_model()
+    user = User.objects.get(username=username)
 
-    with transaction.atomic():
-        user = user.objects.get(username=username)
+    order = Order.objects.create(user=user)
 
-        order = Order.objects.create(user=user)
+    if date:
+        order.created_at = parse_datetime(date)
+        order.save(update_fields=["created_at"])
 
-        if date:
-            order.created_at = parse_datetime(date)
-            order.save(update_fields=["created_at"])
+    for ticket_data in tickets:
+        Ticket.objects.create(
+            order=order,
+            movie_session_id=ticket_data["movie_session"],  # ID expected
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+        )
 
-        for ticket_data in tickets:
-            Ticket.objects.create(
-                order=order,
-                movie_session_id=ticket_data["movie_session"],
-                row=ticket_data["row"],
-                seat=ticket_data["seat"],
-            )
-
-        return order
+    return order
 
 
-def get_orders(username: str = None) -> QuerySet:
+def get_orders(username: str = None) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
